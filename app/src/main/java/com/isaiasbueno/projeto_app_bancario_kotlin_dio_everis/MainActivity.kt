546e7ccae272778a43648d73df4fa2f08ac51cfe
IsaiasBueno <izaias.bueno@gmail.com>
2021-04-27 10:13:05 -0300

    Commit/Update 27/04/2021

---
+++ app/src/main/java/com/isaiasbueno/projeto_app_bancario_kotlin_dio_everis/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.isaiasbueno.projeto_app_bancario_kotlin_dio_everis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
